fb65c4f42581a0b7c74260648ec52f842c6bcea7
SanketDG <sanketdasgupta@gmail.com>
2015-08-27 22:53:22 +0530

    load SECRET_KEY from environment variable

--- contributr/contributr/settings.py
+++ contributr/contributr/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+from django.core.exceptions import ImproperlyConfigured
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -20,7 +21,16 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'djkk&8izc3tmgva(m$%u_zvjk8%f@-6w(d-z68@m-@sh3nz#pi'
+
+# Loads SECRET_KEY from environment variable SECRET_KEY
+def get_env_variable(var_name):
+    try:
+        return os.environ[var_name]
+    except KeyError:
+        error_msg = "Set the %s environment variable" % var_name
+        raise ImproperlyConfigured(error_msg)
+
+SECRET_KEY = get_env_variable('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
