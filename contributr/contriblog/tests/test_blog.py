03d3c797b97025aa9a80bd0292d37d06eceb1fa2
Tobias Schönberg <tobias47n9e@gmail.com>
2015-09-05 14:11:30 +0200

    Add blog app with simple view, necessary settings, url-patterns
    This is the first skeleton of the blog app. From the url-root
    all patterns starting with '/blog/' will be redirected to
    the blog. As of now the blog only has one simple view that
    sends a text as HttpResponse. The reachability of the page
    and the unreachability of the subpages is tested. The folder
    contriblog is now part of the INSTALLED_APPS.
    
    Part of: #45

---
+++ contributr/contriblog/tests/test_blog.py
@@ -0,0 +1,26 @@
+import pytest
+
+from django.core.urlresolvers import reverse
+
+@pytest.mark.django_db
+def test_call_blog(client):
+    """
+    Asserts whether the blog page sends a 200 HTTP status code as response
+
+    200 means that the HTTP request was successful. The URL and port
+    may vary. The fixture above the function allows the function to
+    access the database.
+    """
+    response = client.get(reverse("blog:index"))
+    assert response.status_code == 200
+
+@pytest.mark.django_db
+def test_call_blog_subpage(client):
+    """
+    Asserts whether subpages of the blog page 404
+
+    404 means that no page is returned. This is the expected behaviour,
+    because we don't have subpages for the blog at the moment.
+    """
+    response = client.get(reverse("blog:index") + "december")
+    assert response.status_code == 404
--- contributr/contributr/urls.py
+++ contributr/contributr/urls.py
@@ -18,4 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^blog/', include("contriblog.urls", namespace="blog"))
 ]
