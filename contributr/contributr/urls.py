2a05319672fc136184cbc921ed95dbde84ed2941
Andreas <ameistad@gmail.com>
2015-09-10 17:41:19 +0200

    Add base template, frontpage and view.
    Created a site frontpage which inherits from a base template. The base template was created with initializr.com and uses Bootstrap CSS.
    
    Moved static files settings from production to base, so static files are served in development.
    
    Edited tests to assert if localhost:8000 sends the 200 status code.
    
    Closes #59

--- contributr/contributr/urls.py
+++ contributr/contributr/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.views.generic import TemplateView
 
 urlpatterns = [
+    url(r'^$', TemplateView.as_view(template_name='contributr/index.html'), name="home"),
     url(r'^admin/', include(admin.site.urls)),
 ]
