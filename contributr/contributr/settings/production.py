225b70c0b285257c8b723cbd1f7cbc66154a908c
SanketDG <sanketdasgupta@gmail.com>
2015-10-01 00:02:33 +0530

    set ACCOUNT_DEFAULT_HTTP_PROTOCOL to https
    doing this allows us to use https (SSL) on callback urls while using
    oAuth2 with django-allauth without any quirks.
    
    fixes #87

--- contributr/contributr/settings/production.py
+++ contributr/contributr/settings/production.py
@@ -2,7 +2,6 @@
 # This file contains settings for the Heroku production server
 
 from .base import *
-import os
 
 # Parse database configuration from $DATABASE_URL in heroku environment
 import dj_database_url
@@ -15,3 +14,6 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 ALLOWED_HOSTS = ['*']
 
 DEBUG = True
+
+# Allow https protocol to be used with callback urls in allauth
+ACCOUNT_DEFAULT_HTTP_PROTOCOL = "https"
